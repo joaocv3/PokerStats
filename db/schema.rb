f7ee823f9998ea53309f732a98c199a48ee7d975
Joao C Vieira <joaocv3@gmail.com>
2017-03-26 22:57:49 -0300

    chore: Add Player model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170327010450) do
+ActiveRecord::Schema.define(version: 20170327015507) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "name"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 20170327010450) do
     t.integer "group_id"
     t.index ["group_id"], name: "index_groups_users_on_group_id"
     t.index ["user_id"], name: "index_groups_users_on_user_id"
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id"], name: "index_players_on_group_id"
   end
 
   create_table "users", force: :cascade do |t|
